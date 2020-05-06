21df6603e095ea88489a4b4f9fd097d9ce73084b
jsong073 <jaehasong073@gmail.com>
2020-05-06 15:16:36 -0400

    modified cars

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
         Car audi = new Car("black", "Audi");
         Car mercedes = new Car("silver", "Mercedes");
+        /*
         System.out.println(audi.toString());
         System.out.println(mercedes.toString());
         System.out.println(audi.startEngine());
@@ -15,6 +20,25 @@ public class App {
         System.out.println(audi.decelerate());
         System.out.println(mercedes.stopEngine());
         System.out.println(audi.stopEngine());
+         */
+        cars.add(audi);
+        cars.add(mercedes);
+        String response = "";
+        do {
+            Car car = new Car();
+            System.out.print("Enter the name of the new car: ");
+            car.setMake(input.nextLine());
+            System.out.println("Enter the color of the new car: ");
+            car.setColor(input.nextLine());
+            cars.add(car);
+            System.out.print("Add another car? y/n: ");
+            response = input.nextLine();
+        } while (!response.equalsIgnoreCase("n"));
+
+        System.out.println("Your cars: ");
+        for (Car car: cars) {
+            System.out.println(car.toString());
+        }
 
 
 
